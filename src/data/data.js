6610634a8836638566da73db09e6dafb2f659561
CHADLIZakaria <zakaria.chadli05@gmail.com>
2022-04-27 11:38:35 +0000

    Add data file (rams, ssd, hdd ...)

---
+++ src/data/data.js
@@ -0,0 +1,14 @@
+export const rams = [ 
+    "1 Go", 
+    "2 Go",
+    "3 Go", 
+    "4 Go",
+    "8 Go", 
+    "16 Go",
+    "32 Go",
+    "43 Go", 
+    "64 Go",   
+]
+export const ssd = [
+    
+]
